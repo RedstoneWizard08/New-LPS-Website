d17c987f5085b358a6ab7089974b17f083840a79
Jacob Sapoznikow <jacob1coder@gmail.com>
2024-09-05 02:47:50 +0000

    More!

--- src/api/curseforge.ts
+++ src/api/curseforge.ts
@@ -3,6 +3,8 @@ import { CURSEFORGE_USER_ID, PAGINATION_SIZE } from "../config";
 import { CurseForgeMod } from "./schema/curseforge/mod";
 import type { CurseForgePaginatedResponse } from "curseforge-api/v1/Client";
 
+let requests = 0;
+
 export const getModDescription = async (id: number): Promise<string> => {
     return (
         await fetch(`https://api.curseforge.com/v1/mods/${id}/description`, {
@@ -13,15 +15,20 @@ export const getModDescription = async (id: number): Promise<string> => {
     ).data;
 };
 
-export const getModsForUser = async (
+const getModsForUserWithClass_ = async (
+    user: number,
     index: number,
+    classId: number,
 ): Promise<CurseForgePaginatedResponse<CurseForgeMod>> => {
+    requests++;
+
     const url = new URL("https://api.curseforge.com/v1/mods/search");
 
     url.searchParams.set("gameId", (432).toString());
-    url.searchParams.set("authorId", CURSEFORGE_USER_ID.toString());
+    url.searchParams.set("primaryAuthorId", user.toString());
     url.searchParams.set("pageSize", PAGINATION_SIZE.toString());
     url.searchParams.set("index", (index * PAGINATION_SIZE).toString());
+    url.searchParams.set("classId", classId.toString());
 
     const { data, pagination }: { data: any[]; pagination: any } = await fetch(
         url,
@@ -38,14 +45,17 @@ export const getModsForUser = async (
     };
 };
 
-export const getAllCurseProjects = async () => {
+export const getModsForUserWithClass = async (
+    user: number,
+    classId: number,
+): Promise<CurseForgeMod[]> => {
     const projs = [];
 
     let page = 0;
     let max = 1;
 
     while (page < max) {
-        const data = await getModsForUser(page);
+        const data = await getModsForUserWithClass_(user, page, classId);
 
         projs.push(...data.data);
 
@@ -54,4 +64,44 @@ export const getAllCurseProjects = async () => {
     }
 
     return projs;
+};
+
+export const getAllCurseProjects = async (): Promise<CurseForgeMod[]> => {
+    requests = 0;
+
+    const users = [
+        CURSEFORGE_USER_ID,
+        103001449, // ElocinDev
+        10384423, // Fuzs
+        101731217, // Aureljz
+        101032666, // y4z0n
+        101555282, // SHXRKIE
+        100442098, // SideralisMusic
+        // 103789844, // MuonR
+        102600854, // mim1q
+        100999484, // chorb
+        // 102939973, // quickqwek
+        // 101476500, // Nyagi_Byte
+    ];
+
+    const classes = [6, 4471, 6945, 12, 4559, 6552];
+    const items: CurseForgeMod[] = [];
+
+    for (const user of users) {
+        for (const cl of classes) {
+            for (const item of await getModsForUserWithClass(user, cl)) {
+                if (!items.find((v) => v.id == item.id)) {
+                    console.log(
+                        `${item.name}: ${item.authors.map((v) => v.name).join(", ")}`,
+                    );
+
+                    items.push(item);
+                }
+            }
+        }
+    }
+
+    console.log(`Fetched CurseForge projects in ${requests} requests.`);
+
+    return items;
 };
