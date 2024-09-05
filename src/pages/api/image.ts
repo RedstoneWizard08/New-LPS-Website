c4fc9b97dc6547169f9e385494b72ba372b893bd
Jacob Sapoznikow <jacob1coder@gmail.com>
2024-09-05 01:59:13 +0000

    Mobile, new logo, and more!

--- src/pages/api/image.ts
+++ src/pages/api/image.ts
@@ -11,8 +11,19 @@ const IMAGE_TYPES = [
 ];
 
 export const GET: APIRoute = async ({ url }) => {
-    const img = url.searchParams.get("url")!;
-    const res = await fetch(decodeURIComponent(img));
+    const img = decodeURIComponent(url.searchParams.get("url")!);
+    const res = await fetch(img);
+
+    if (img.startsWith("https://i.imgur.com/")) {
+        const spl = img.split(".");
+        const ext = spl[spl.length - 1];
+
+        return new Response(await res.arrayBuffer(), {
+            headers: {
+                "Content-Type": `image/${ext}`,
+            },
+        });
+    }
 
     if (IMAGE_TYPES.includes(res.headers.get("Content-Type")!))
         return new Response(await res.arrayBuffer(), {
