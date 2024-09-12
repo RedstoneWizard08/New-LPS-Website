5b58c81983d118864b482419fb16d981ad79f9cb
Jacob Sapoznikow <jacob1coder@gmail.com>
2024-09-12 22:59:52 +0000

    Fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,21 @@
-import adapter from "@sveltejs/adapter-auto";
+import node from "@sveltejs/adapter-node";
+import vercel from "@sveltejs/adapter-vercel";
+import cloudflare from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import uno from "@unocss/svelte-scoped/preprocess";
+
+const adapter = process.env.CLOUDFLARE
+    ? cloudflare()
+    : process.env.VERCEL
+      ? vercel()
+      : node();
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
     preprocess: [vitePreprocess(), uno()],
 
     kit: {
-        adapter: adapter(),
+        adapter,
 
         alias: {
             "$/*": "src/*",
